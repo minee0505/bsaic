2bbec4fdd29a92564ae84896e5d0132640d9a504
minee0505 <wodeyue97@gmail.com>
2025-07-11 12:48:47 +0900

    Configure application port, dependencies, and `.gitignore` adjustments.

--- src/main/java/com/spring/bsaic/SpringWebBasic202507Application.java
+++ src/main/java/com/spring/bsaic/SpringWebBasic202507Application.java
@@ -6,7 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringWebBasic202507Application {
 
+
 	public static void main(String[] args) {
+
 		SpringApplication.run(SpringWebBasic202507Application.class, args);
 	}
 
